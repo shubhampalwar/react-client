eaa85bdfb7ca2f0416af28118a7a27be79ddda0c
shubham palwar <shubham.palwar@successive.tech>
2019-02-26 23:49:14 +0530

    feat: validation on login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {
-  withStyles, CssBaseline, Paper, Avatar, Typography, FormControl, TextField, InputAdornment,
+  withStyles, Button, CssBaseline, Paper, Avatar, Typography, TextField, InputAdornment, Grid,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import EmailIcon from '@material-ui/icons/Email';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import * as yup from 'yup';
 
 const styles = theme => ({
   main: {
     width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    display: 'block',
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
@@ -31,13 +32,18 @@ const styles = theme => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
+    // width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit * 2,
   },
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
 });
+
+const validationSchema = yup.object({
+  email: yup.string().email().required().label('Email'),
+  password: yup.string().required().label('Password'),
+})
 
 class Login extends Component {
   constructor(props) {
@@ -45,8 +51,67 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
+      errors: {},
+      touched: {},
     };
   }
+
+  handelChange = field => (event) => {
+    this.setState({
+        [field]: event.target.value,
+      }, () => this.handleValidate());
+  };
+
+  handleSubmit = () => {
+    // const { email, password } = this.state;
+
+    console.log(this.state);
+  }
+
+  handleBlur = field => () => {
+    const { touched } = this.state;
+    touched[field] = true;
+    this.setState({ touched }, () => { console.log(this.state);
+    ; this.handleValidate();});
+  };
+
+  handleValidate = () => {
+    const { email, password } = this.state;
+    validationSchema
+      .validate({ email,password },{ abortEarly: false })
+      .then(() => {
+        this.handleError(null);
+      })
+      .catch((errors) => {
+        this.handleError(errors);
+      });
+  };
+
+  handleError = (errors) => {
+    const allErrors = {};
+    if (errors) {
+      errors.inner.forEach((error) => {
+        allErrors[error.path] = error.message;
+      });
+    }
+  console.log('all error', allErrors);
+
+    this.setState({
+      errors: allErrors,
+    });
+  };
+
+  getErrors = (field) => {
+    const { errors, touched } = this.state;
+
+    if (!touched[field]) {
+      return null;
+    }
+
+    return errors[field] || '';
+  }
+
+  checkState = field => Object.keys(this.state[field]).length !== 0;
 
   renderInputField = (field, label, value, inputType, icon) => (
     <TextField
@@ -62,8 +127,6 @@ class Login extends Component {
       }}
       fullWidth
       error={!!this.getErrors(field)}
-      autoFocus={(field === 'email')}
-      autoComplete={field}
     />
   );
 
@@ -79,8 +142,8 @@ class Login extends Component {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">Login</Typography>
-            <form className={classes.form}>
-              <FormControl required fullWidth>
+            <Grid container spacing={24} className={classes.form}>
+              <Grid item xs={12}>
                 {this.renderInputField(
                   'email',
                   'Email',
@@ -88,8 +151,8 @@ class Login extends Component {
                   'texts',
                   <EmailIcon />,
                 )}
-              </FormControl>
-              <FormControl required fullWidth>
+              </Grid>
+              <Grid item xs={12}>
                 {this.renderInputField(
                   'password',
                   'Password',
@@ -97,8 +160,19 @@ class Login extends Component {
                   'password',
                   <VisibilityOffIcon />,
                 )}
-              </FormControl>
-            </form>
+              </Grid>
+              <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={this.checkState('errors') || !this.checkState('touched')}
+            className={classes.submit}
+            onClick={this.handleSubmit}
+          >
+            Sign in
+</Button>
+            </Grid>
           </Paper>
         </main>
       </>
